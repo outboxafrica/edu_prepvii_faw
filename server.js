28e6159c817c0b47b041c78585b8e82ef582282c
Tracy Nuwagaba <tnuwagaba191@gmail.com>
2020-09-11 01:10:03 +0300

    update

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const http = require('http');
 const app = require('.');
 
+require('dotenv').config()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.use('/questions', QuestionRoute);
+
+app.get('/', (req, res) => {
+  res.send('Hello Welcome!');
+});
+
+//database
+mongoose.connect('mongodb://localhost:27017/faw',{ useNewUrlParser: true,useUnifiedTopology: true });
+const db = mongoose.connection
+db.on('error',(error) => console.error(error))
+db.once('open', () => console.log('Database is connected'))
+
+
+//server
 const port = process.env.PORT || 3000;
-
-
-const server = http.createServer(app);
-
-server.listen(port);
+app.listen(port, (req, res) =>{
+  console.log(`Server is running at http://localhost:${port}`);
+});
+app.use(router);
