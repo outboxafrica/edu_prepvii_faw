0c3b3ab337f3be93f584c2b03b4449bfb0289f26
TracyNuwagaba <tnuwagaba191@gmail.com>
2020-10-26 12:52:29 +0300

    configure cors connectivity to front end

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const morgan = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
+var corsOptions = { 
+  origin: 'http://localhost:5000',           
+  optionsSuccessStatus: 200 
+} 
 
 // import routes
 const authRoutes = require('./routes/authRoute');
@@ -17,6 +22,7 @@ const port = process.env.PORT || 5000;
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors("http://localhost:3000"))
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
@@ -29,7 +35,7 @@ app.use(function (req, res, next) {
 
 // routes
 app.use("/api", authRoutes, answerRoutes, questionRoutes);
-app.get("/", (req, res,) => {
+app.get("/", cors(corsOptions), (req, res, next) => {
     res.status(200).json({
     message: "Welcome to EDU-API for Team FAW. It is a Q&A platform where people can ask questions and give in responses.",
   });
@@ -39,7 +45,7 @@ app.get("/", (req, res,) => {
 const db = process.env.MONGO_URL_Atlas 
 
 //connecting to the database
-mongoose.connect(db, {useUnifiedTopology:true, useNewUrlParser:true, useFindAndModify: false })
+mongoose.connect('mongodb+srv://Priscilla:Priscilla2@faw.hlyy4.mongodb.net/<FAW>?retryWrites=true&w=majority', {useUnifiedTopology:true, useNewUrlParser:true, useFindAndModify: false })
     .then(() => console.log('     MongoDb Connected!!! (*_*) '))
 
 
